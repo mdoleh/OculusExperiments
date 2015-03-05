f1f3c24e894069c83b3e723a61394c32bbf84c1a
Mohammad Doleh <dammahom59@aim.com>
2015-03-04 21:08:45 -0500

    tight rope walking

---
+++ Assets/Scripts/TightRope/Position.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Position : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.transform.position = new Vector3(-2.73f, 61.89f, 10.61f);
+    }
+}
