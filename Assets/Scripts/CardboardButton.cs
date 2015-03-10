3add58fabdfc5e50fc26c401722b9c4ee9a44648
Mohammad Doleh <dammahom59@aim.com>
2015-03-09 20:03:06 -0400

    Menu for cardboard demos

---
+++ Assets/Scripts/CardboardButton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CardboardButton : MonoBehaviour {
+    
+    public GameObject[] buttons;
+
+	void Update () {
+        var ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 1000))
+        {
+            if (hit.collider.transform.name.ToLower().Contains("button"))
+            {
+                SetColorToYellow(hit.collider.gameObject);
+            }
+            else
+            {
+                ResetBackgroundColors();
+            }
+        }
+	}
+
+    private void ResetBackgroundColors()
+    {
+        foreach (var button in buttons)
+        {
+            button.GetComponent<Image>().color = Color.white;
+        }
+    }
+
+    private void SetColorToYellow(GameObject button)
+    {
+        button.GetComponent<Image>().color = Color.yellow;
+    }
+}
